f77f965ce77d45c32945445bd1a0757a4a1d3741
Oscar Cortez <oacortez98@gmail.com>
2022-03-03 14:35:39 -0700

    Completed to display cards

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Card from './Card'
+import '../Styles/HomePage.scss'
 
 
 const HomePage = ({ allGames }) => {
 
+  console.log(allGames);
+
+  const allGameCards = allGames.map((game, index) => {
+    return (
+      <Card 
+        id={index + 1}
+        key={index + 1}
+        image={game.thumb}
+        title={game.title}
+        salePrice={game.salePrice}
+        dealRating={game.dealRating}
+      />
+    )
+  })
+
   return (
-    <div>
-      <h2>HomePage</h2>
-      <Card />
+    <div >
+      <h2 className='home-view-title'>All Deal games</h2>
+    <div className='wrapper'>
+      {allGameCards}
+    </div>
     </div>
   )
 }
