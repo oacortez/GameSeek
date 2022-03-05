c3912b671b77bb7190d7d04d09ce23b7d57ca3e8
Oscar Cortez <oacortez98@gmail.com>
2022-03-05 11:27:52 -0700

    Refactor code for styling

--- src/Components/FavoritesList.js
+++ src/Components/FavoritesList.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import '../Styles/FavoritesList.scss';
 
 const FavoritesList = ({ favoriteGames,  removeFavorite}) => {
 
   const userFavoriteGames = favoriteGames.map(gameInfo => {
-    return ( 
+    return (
+      <div className='favorite-card-container'>
         <div className='favorite-card' key={gameInfo.dealID}>
           <img src={gameInfo.thumb} className='favorite-card-image'/>
           <h2 className='favorite-card-title'>{gameInfo.title}</h2>
           <p className='favorite-card-price'>Sale Price: ${gameInfo.salePrice}</p>
           <p className='favorite-card-rating'>Deal Rating: {gameInfo.dealRating}/10</p>
-          <button onClick={() => removeFavorite(gameInfo.dealID)}>Remove from favorites</button>
         </div>
+          <button className='remove-favorites-btn' onClick={() => removeFavorite(gameInfo.dealID)}>Remove from favorites</button>
+      </div> 
     )
   })
   
   return (
-    <div className='favorite-card-container'>
-        {!userFavoriteGames.length ? <h1>You don't have any favorites</h1> : userFavoriteGames}
+    <div>
+      {!userFavoriteGames.length ? <h1 className='no-favorites-msg'>You don't have any favorites</h1> : <h1 className='favorites-list-title'> My favorites List </h1>}
+      <div className='wrapper'>
+      {userFavoriteGames}
+       </div>
     </div>
   )
 }
