889486457fe379e4b4561bc6beacfc3c795b41d1
Oscar Cortez <oacortez98@gmail.com>
2022-03-06 20:34:44 -0700

    Completed All cypress test

---
+++ cypress/integration/errorHandling.js
@@ -0,0 +1,19 @@
+describe('Error Handling', () => {
+  beforeEach(() => {
+    cy.intercept('GET','https://www.cheapshark.com/api/1.0/deals', {
+      statusCode: 500
+    })
+  })
+  
+  it('Should display error message for 500 status code', () => {      
+      cy.visit('http://localhost:3000/***')
+      .get('h4')
+        .contains('Sorry we were unable to find this resource for you! Please try again')
+    });
+
+  it('Should display error message for 500 status code', () => {
+      cy.visit('http://localhost:3000/favorites/**')
+      cy.get('.error-message')
+      .contains('Sorry we were unable to find this resource for you! Please try again')
+  });
+});
