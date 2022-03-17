80dc83aa8f84bf20ab50fd393194eaace8856379
Oscar Cortez <oacortez98@gmail.com>
2022-03-17 14:36:55 -0600

    Adjust testing file

--- cypress/integration/errorHandling.js
+++ cypress/integration/errorHandling.js
@@ -3,16 +3,15 @@ describe('Error Handling', () => {
     cy.intercept('GET','https://www.cheapshark.com/api/1.0/deals', {
       statusCode: 500
     })
-    cy.visit('http://localhost:3000/')
   })
   
-  it('Should display error message for 500 status code', () => {      
-      cy.visit('http://localhost:3000/***')
+  it('Should display error message for 500 status code in home view', () => {      
+      cy.visit('http://localhost:3000/error')
       .get('h4')
         .contains('Sorry we were unable to find this resource for you! Please try again')
     });
 
-  it('Should display error message for 500 status code', () => {
+  it('Should display error message for 500 status code in favorites view', () => {
       cy.visit('http://localhost:3000/favorites/**')
       cy.get('.error-message')
         .contains('Sorry we were unable to find this resource for you! Please try again')
