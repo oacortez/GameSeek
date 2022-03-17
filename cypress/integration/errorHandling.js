06fc8009db1c73a0a53e7f43c91da731d4c7caf4
Oscar Cortez <oacortez98@gmail.com>
2022-03-17 14:32:47 -0600

    Test error handling file ci

--- cypress/integration/errorHandling.js
+++ cypress/integration/errorHandling.js
@@ -3,6 +3,7 @@ describe('Error Handling', () => {
     cy.intercept('GET','https://www.cheapshark.com/api/1.0/deals', {
       statusCode: 500
     })
+    cy.visit('http://localhost:3000/')
   })
   
   it('Should display error message for 500 status code', () => {      
@@ -14,6 +15,6 @@ describe('Error Handling', () => {
   it('Should display error message for 500 status code', () => {
       cy.visit('http://localhost:3000/favorites/**')
       cy.get('.error-message')
-      .contains('Sorry we were unable to find this resource for you! Please try again')
+        .contains('Sorry we were unable to find this resource for you! Please try again')
   });
 });
